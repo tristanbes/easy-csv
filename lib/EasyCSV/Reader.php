90b8dd72b47c3d93ecb6609d2e2ed66ab90694d7
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2012-11-16 09:57:56 +0100

    Apply some patches from Chentepixtol fork

--- lib/EasyCSV/Reader.php
+++ lib/EasyCSV/Reader.php
@@ -2,28 +2,128 @@
 
 namespace EasyCSV;
 
-class Reader extends AbstractBase
+/**
+ *
+ * @author chente
+ */
+class Reader extends AbstractBase implements \Iterator
 {
+
     private $_headers;
     private $_line;
+    private $currentRow;
+    private $isValid = false;
+    private $firsRow;
 
-    public function __construct($path, $mode = 'r+', $delimiter = ',', $enclosure = '"')
+    /**
+     *
+     * @param unknown_type $path
+     * @param unknown_type $mode
+     */
+    public function __construct($path, $mode = 'r+')
     {
-        parent::__construct($path, $mode, $delimiter, $enclosure);
-        $this->_headers = $this->getRow();
-        $this->_line    = 0;
+        parent::__construct($path, $mode);
+        $this->initializate();
     }
 
+    /**
+     *
+     */
+    private function initializate(){
+        $this->currentRow = null;
+        $this->_headers = $this->fgetcsv();
+        $this->isValid = $this->_headers !== false;
+        if( is_array($this->_headers) ){
+            $this->_headers = array_map('strtolower', $this->_headers);
+        }
+        $this->_line = 1;
+    }
+
+    /**
+     * @return array
+     */
+    protected function fgetcsv(){
+        return fgetcsv($this->_handle, 1000, $this->_delimiter, $this->_enclosure);
+    }
+
+    /**
+     *
+     * @return array
+     */
     public function getRow()
     {
-        if (($row = fgetcsv($this->_handle, 1000, $this->_delimiter, $this->_enclosure)) !== false) {
+        if( ($row = $this->fgetcsv()) !== false ) {
+            $this->isValid = true;
             $this->_line++;
-            return $this->_headers ? array_combine($this->_headers, $row) : $row;
+            $this->currentRow = $this->getCombineRow($row);
+            return $this->currentRow;
         } else {
-            return false;
+            $this->isValid = false;
+            return $this->isValid;
         }
     }
 
+    /**
+     *
+     * @param array $row
+     */
+    private function getCombineRow($row){
+        $newRow = array();
+        foreach( $this->_headers as $i => $name ){
+            if( !isset($row[$i]) ){
+                $newRow[$i] = null;
+            }else{
+                $newRow[$i] = $row[$i];
+            }
+        }
+        return array_combine($this->_headers, $newRow);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Iterator::current()
+     */
+    public function current(){
+        return null == $this->currentRow ? $this->getRow() : $this->currentRow;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Iterator::key()
+     */
+    public function key(){
+        return $this->_line;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Iterator::next()
+     */
+    public function next(){
+        $this->getRow();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Iterator::rewind()
+     */
+    public function rewind(){
+        $this->closeFile();
+        $this->openFile();
+        $this->initializate();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Iterator::valid()
+     */
+    public function valid(){
+        return $this->isValid;
+    }
+
+    /**
+     * @return array
+     */
     public function getAll()
     {
         $data = array();
@@ -33,8 +133,18 @@ class Reader extends AbstractBase
         return $data;
     }
 
-    public function getLineNumber()
-    {
+    /**
+     *
+     * @return number
+     */
+    public function getLineNumber(){
         return $this->_line;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getHeaders(){
+        return $this->_headers;
     }
 }
--- tests/EasyCSV/Tests/ReaderTest.php
+++ tests/EasyCSV/Tests/ReaderTest.php
@@ -2,27 +2,65 @@
 
 namespace EasyCSV\Tests;
 
-require_once __DIR__ . '/TestInit.php';
 
 class ReaderTest extends \PHPUnit_Framework_TestCase
 {
-    private $_reader;
-
-    public function setUp()
-    {
-        $this->_reader = new \EasyCSV\Reader(__DIR__ . '/read.csv');
-    }
-
     public function testOneAtAtime()
     {
-        while($row = $this->_reader->getRow()) {
+        $reader  = $this->getReader();
+        while($row = $reader->getRow()) {
             $this->assertTrue(is_array($row));
-            $this->assertEquals(3, count($row));
+            $this->assertEquals(4, count($row));
         }
     }
 
     public function testGetAll()
     {
-        $this->assertEquals(5, count($this->_reader->getAll()));
+        $this->assertEquals(5, count($this->getReader()->getAll()));
+    }
+
+    public function testOthet(){
+        $reader  = $this->getReader();
+        print_r($reader->getAll());die();
+    }
+
+    /**
+     * @test
+     */
+    public function iterator()
+    {
+        $iterator = $this->getReader();
+        $rowsWhile = array();
+        $line = 1;
+        while( $iterator->valid() ) {
+            $row = $iterator->current();
+            $line++;
+            $rowsWhile[] = $row;
+
+            $this->assertEquals(array("column1", "column2", "column3", 'column4'), array_keys($row));
+            $this->assertEquals($line, $iterator->key());
+
+            $iterator->next();
+        }
+        $iterator->rewind();
+
+        $this->assertEquals(1, $iterator->getLineNumber());
+        $rowsFor = array();
+        $line = 1;
+        foreach($iterator as $key => $row){
+            $line++;
+            $rowsFor[] = $row;
+            $this->assertEquals(array("column1", "column2", "column3", 'column4'), array_keys($row));
+            $this->assertEquals($line, $key);
+        }
+
+        $this->assertEquals($rowsWhile, $rowsFor);
+    }
+
+    /**
+     * @return \EasyCSV\Reader
+     */
+    private function getReader(){
+        return new \EasyCSV\Reader(__DIR__ . '/mocks/read.csv');
     }
 }
